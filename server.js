b1b0228854be4d648dff726cf64e94e349293e6a
cherjios <slopez0286@gmail.com>
2020-08-04 16:33:48 -0700

    Adding server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var path = require("path");
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
